f84414c72d3832310a6148c3e493a4ba07a8f3c7
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2021-11-01 13:28:57 +0000

    Add content to the landing page
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>

---
+++ landing/README.md
@@ -0,0 +1,7 @@
+# Landing Page for future recaptime.app
+
+Once the app is launched succesfully, this would be replace the GitLab group-level Pages stuff in the future.
+
+## Available Commands
+
+The usual `dev`, `build`, and `server` commands are available, just make sure to prefix it with `landing:`.
---
+++ landing/pages/index.js
@@ -0,0 +1,183 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Coming Soon | Recap Time</title>
+      </Head>
+
+      <main>
+        <h1 className="title">
+         Coming soon on <b>recaptime.app!</b>
+        </h1>
+
+        <p className="description">
+          One app to access Matrix + ActivityPub-compliant apps on the fediverse
+        </p>
+
+        <div className="grid">
+          <a href="https://gitlab.com/RecapTime/app/blob/main/CONTRIBUTING.md" className="card">
+            <h3>Help us develop &rarr;</h3>
+            <p>We're currently in the early stages of the app development. Ideas are welcome!</p>
+          </a>
+
+          <a href="https://mastodon.online/@RecapTime" className="card">
+            <h3>Follow us on fediverse &rarr;</h3>
+            <p>Don't like Twitter. We got you covered!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>Follow us on Twitter &rarr;</h3>
+            <p>Not in the fediverse? No problemo, we're not going anywhere.</p>
+          </a>
+
+          <a
+            href="/join"
+            className="card"
+          >
+            <h3>More from Recap Time squad &rarr;</h3>
+            <p>
+              Explore more OSS projects we maintain on GitLab.com. Also, consider joining us too.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          Built with love by Andrei Jiroh with Next.js and Vercel
+        </p>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+        .logo {
+          height: 1em;
+        }
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
