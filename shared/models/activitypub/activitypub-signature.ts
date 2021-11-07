70f38de849de8b1270518841f7f9cc276d7320f2
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2021-11-07 13:26:19 +0000

    global: Slap copyright headers for AGPL compliance
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>

--- shared/models/activitypub/activitypub-signature.ts
+++ shared/models/activitypub/activitypub-signature.ts
@@ -1,3 +1,9 @@
+// Copyright © 2021 Andrei Jiroh Halili and contributors. Licensed under AGPLv3 or later.
+// Portions of code is borrowed from PeerTube's source code which can be available at
+// https://github.com/Chocobozzz/PeerTube, which licensed under AGPLv3, among others which
+// can be found in the CREDITS.md file at root directory of this source tree.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export interface ActivityPubSignature {
     type: string
     created: Date
